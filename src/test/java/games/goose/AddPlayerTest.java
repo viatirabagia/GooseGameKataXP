548622d07521e7255bebfb5dd11e76873f370f36
viatirabagia <via.tirabagia@tiscali.it>
2021-04-29 17:32:18 +0200

    Add Players Test

---
+++ src/test/java/games/goose/AddPlayerTest.java
@@ -0,0 +1,21 @@
+package games.goose;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AddPlayerTest {
+
+    @Test
+    public void addNewPlayer()
+    {
+        assertTrue( false );
+    }
+
+    @Test
+    public void addExistingPlayer()
+    {
+        assertTrue( false );
+    }
+
+}
