08fe1ab80b34afbcc515e038b142d62983cc4d64
viatirabagia <via.tirabagia@tiscali.it>
2021-04-29 18:21:12 +0200

    Move player test passed

--- src/main/java/games/goose/Game.java
+++ src/main/java/games/goose/Game.java
@@ -1,18 +1,21 @@
 package games.goose;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
 
     private List<String> players = new ArrayList<>();
+    private Map<String, Integer> positions = new HashMap<>();
 
     /**
      *
      * @param player
      * @return true if added, false otherwise
      */
-    public boolean addPlayer(String player) {
+    public boolean addPlayer(final String player) {
         if (player == null || players.contains(player)) {
             return false;
         }
@@ -24,14 +27,26 @@ public class Game {
         return players.size();
     }
 
-    public boolean existPlayer(String player) {
+    public boolean existPlayer(final String player) {
         return player==null ? false : players.contains(player);
     }
 
     public void move(String player, int diceOne, int diceTwo) {
+        int currentPosition = playerPosition(player);
+        positions.put(player, nextPosition(currentPosition, diceOne, diceTwo));
+
     }
 
-    public int playerPosition(String player) {
-        return -1;
+    private Integer nextPosition(int currentPosition, int diceOne, int diceTwo) {
+        return currentPosition + diceOne + diceTwo;
     }
+
+    public int playerPosition(final String player) {
+        Integer currentPosition = positions.get(player);
+        if (currentPosition == null) {
+            return 0;
+        }
+        return currentPosition;
+    }
+
 }
