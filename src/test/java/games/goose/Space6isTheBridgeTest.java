241f7e9b51a62c67f6570b64a32fd7091231f1de
viatirabagia <via.tirabagia@tiscali.it>
2021-04-29 22:23:26 +0200

    Space 6 is the bridge fixed

--- src/test/java/games/goose/Space6isTheBridgeTest.java
+++ src/test/java/games/goose/Space6isTheBridgeTest.java
@@ -10,7 +10,7 @@ public class Space6isTheBridgeTest {
     @Test
     public void space6isTheBridge() {
         MoveRules rules = new GooseMoveRules();
-        MoveRules.Result result = rules.nextPosition(0, 2, 4);
+        MoveRules.Result result = rules.nextPosition(4, 1, 1);
         assertEquals(12, result.position);
     }
 }
