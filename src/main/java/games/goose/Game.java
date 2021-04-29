423b6541952d4bd5259fadd4607426ae6e1f4b1e
viatirabagia <via.tirabagia@tiscali.it>
2021-04-29 17:43:43 +0200

    Add player test fail

---
+++ src/main/java/games/goose/Game.java
@@ -0,0 +1,12 @@
+package games.goose;
+
+public class Game {
+    public void addPlayer(String pippo) {
+    }
+
+    public int getPlayersCount() {
+    }
+
+    public boolean existPlayer(String pippo) {
+    }
+}
--- src/test/java/games/goose/AddPlayerTest.java
+++ src/test/java/games/goose/AddPlayerTest.java
@@ -1,5 +1,6 @@
 package games.goose;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,15 +8,31 @@ import org.junit.Test;
 public class AddPlayerTest {
 
     @Test
-    public void addNewPlayer()
+    public void addFirstPlayer()
     {
-        assertTrue( false );
+        Game g = new Game();
+        g.addPlayer("Pippo");
+        assertEquals(1, g.getPlayersCount());
+        assertTrue(g.existPlayer("Pippo"));
+    }
+
+    @Test
+    public void addSecondPlayer()
+    {
+        Game g = new Game();
+        g.addPlayer("Pippo");
+        g.addPlayer("Pluto");
+        assertEquals(2, g.getPlayersCount());
+        assertTrue(g.existPlayer("Pluto"));
     }
 
     @Test
     public void addExistingPlayer()
     {
-        assertTrue( false );
+        Game g = new Game();
+        g.addPlayer("Pippo");
+        g.addPlayer("Pippo");
+        assertEquals(1, g.getPlayersCount());
     }
 
 }
