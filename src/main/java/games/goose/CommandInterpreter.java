4f3f2ba6e0c8e0179bc59677590b890a017cb9bf
viatirabagia <via.tirabagia@tiscali.it>
2021-04-30 08:13:55 +0200

    Added command interpreter test - test fails

---
+++ src/main/java/games/goose/CommandInterpreter.java
@@ -0,0 +1,4 @@
+package games.goose;
+
+public class CommandInterpreter {
+}
---
+++ src/test/java/games/goose/CommandInterpreterTest.java
@@ -0,0 +1,30 @@
+package games.goose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandInterpreterTest {
+
+    @Test
+    public void addPlayer()
+    {
+        CommandInterpreter i = new CommandInterpreter();
+        assertTrue(false);
+    }
+    @Test
+    public void movePlayer()
+    {
+        CommandInterpreter i = new CommandInterpreter();
+        assertTrue(false);
+    }
+
+    @Test
+    public void movePlayerGameThrowsTheDice()
+    {
+        CommandInterpreter i = new CommandInterpreter();
+        assertTrue(false);
+    }
+
+
+}
